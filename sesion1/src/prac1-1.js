b451e4a0a4c2d562729058b77b6d014a2edfc24c
Joel Pevida Menéndez <71332752+uo265213@users.noreply.github.com>
2022-02-08 20:56:49 +0100

    reinicio del repositorio

--- sesion1/src/prac1-1.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from 'three';
-import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
-
-if ( WEBGL.isWebGLAvailable() ) {
-    console.log("WEBGL soportado en el nagevador ");
-}
