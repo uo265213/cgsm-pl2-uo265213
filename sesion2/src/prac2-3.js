6cd47db7804c0636290d9ee1005d8792507adb4c
uo265213 <uo265213@uniovi.es>
2022-02-10 17:51:22 +0100

    Practica 2 hasta el punto 3(no incluido) v2

---
+++ sesion2/src/prac2-3.js
@@ -0,0 +1,62 @@
+import * as THREE from 'three';
+import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
+
+if ( WEBGL.isWebGLAvailable() ) {
+    console.log("WEBGL soportado en el nagevador ");
+}
+
+window.addEventListener( 'resize', ( ) => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix( );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+}, false );
+
+const scene = new THREE.Scene();
+
+const renderer = WEBGL.isWebGLAvailable() ? new THREE.WebGLRenderer( {antialias: true} ) : new THREE.CanvasRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+camera.position.set( 0, 0, 4 );
+
+const spheregeometry = new THREE.SphereGeometry( 1 );
+
+//const spherematerial = new THREE.MeshBasicMaterial( );
+const mapUrl = "../textures/tierra.gif";   // The file used as texture
+const textureLoader = new THREE.TextureLoader( );  // The object used to load textures
+const map = textureLoader.load( mapUrl, ( loaded ) => { renderer.render( scene, camera ); } );
+const spherematerial = new THREE.MeshPhongMaterial( { map: map } );
+
+const sphere = new THREE.Mesh( spheregeometry, spherematerial );
+sphere.position.set(0,0,0);
+
+
+
+
+const atmosferageometry = new THREE.SphereGeometry( 1 );
+
+//const spherematerial = new THREE.MeshBasicMaterial( );
+const mapUrlatmosffera = "../textures/atmosfera.gif";   // The file used as texture
+//const textureLoader = new THREE.TextureLoader( );  // The object used to load textures
+const mapatmosfera = textureLoader.load( mapUrlatmosffera, ( loaded ) => { renderer.render( scene, camera ); } );
+//const atmosferamaterial = new THREE.MeshLambertMaterial( { map: mapatmosfera } );
+
+var atmosferamaterial = new THREE.MeshLambertMaterial( { color: 0xFFFFFF, map: mapatmosfera, transparent: true } );
+
+const atmosfera = new THREE.Mesh( atmosferageometry, atmosferamaterial );
+atmosfera.position.set(0,0,0);
+
+
+
+const light = new THREE.PointLight( 0xffffff, 2 );
+light.position.set( 5, 0, 0 );
+scene.add( light );
+
+
+scene.add( sphere );
+scene.add( atmosfera );
+
+renderer.render( scene, camera );
+
---
+++ sesion2/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: "production",
+    entry: {
+        "prac2-1": './src/prac2-1.js',
+        "prac2-2": './src/prac2-2.js',
+        "prac2-3": './src/prac2-3.js',
+        "prac2-4": './src/prac2-4.js',
+        "prac2-5": './src/prac2-5.js'
+        
+    },
+    devServer: {
+        static: {
+            directory: __dirname
+        },
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    performance: {
+        maxAssetSize: 2000000,
+        maxEntrypointSize: 2000000
+    }
+};
